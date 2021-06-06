827a6cb2f3192caa32d1055780f5630a9be84ebb
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-06-06 19:33:12 +0200

    Update packing.py

--- packing.py
+++ packing.py
@@ -2,12 +2,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                                              *  2-Dimensional Case Picking  *
 
-In this file are implemented many 2-dimensional packing algorithms (or strip packing algorithms). They have been
-written and designed to be implemented in the 2-Dimensional Case Picking algorithm that comes from the collaboration
-between University of Parma, University of Modena, and Universitat Oberta de Catalunya.
-
-All algorithms are taken from Jukka Jylang (2010) - A Thousand Ways to Pack the Bin: A Practical
-Approach to Two-Dimensional Rectangle Bin Packing.
+In this file are implemented many 2-dimensional packing algorithms (or strip packing algorithms). All algorithms are 
+taken from Jukka Jylang (2010) - A Thousand Ways to Pack the Bin: A Practical Approach to Two-Dimensional Rectangle 
+Bin Packing.
 
 Author: Mattia Neroni, Ph.D., Eng. (May 2021).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
